7b18a5b07574c4f930039522af7fe08505e44e6e
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-11-13 07:17:16 +0600

    update blog function added

--- index.js
+++ index.js
@@ -9,30 +9,17 @@ const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 // middleware
-app.use(
-  cors({
-    //     origin: [
-    //       "https://car-doctor-5f3ce.web.app",
-    //       "https://car-doctor-5f3ce.firebaseapp.com"
-    //     //   "http://localhost:5173",
-    //     // "http://localhost:5174"
-    //   ],
-    //     credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
-// app.use(cookieParser());
 
+// routes
 app.get("/", (req, res) => {
   res.send("Blog Zone Server is Running");
 });
 
-
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fesbo2h.mongodb.net/?retryWrites=true&w=majority`;
+  "mongodb+srv://abdullahalrakib30:DlmlfbZ7Rh9nrNJ0@cluster0.fesbo2h.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -45,6 +32,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    app.get("/", (req, res) => {
+      res.send("Blog Zone Server is Running");
+    });
+
     const blogsCollection = client.db("Blogs").collection("allBlogs");
 
     // const bookingCollection = client.db("Blogs").collection("bookings");
@@ -62,8 +53,39 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/blogsCount", async (req, res) => {
+      const count = await blogsCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
+
+    app.get("/all/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/all/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedBlog = req.body;
+      console.log("updated blog", updatedBlog);
+      const blog = {
+        $set: {
+          title: updatedBlog.title,
+          photoUrl: updatedBlog.photoUrl,
+          category: updatedBlog.category,
+          shortDescription: updatedBlog.shortDescription,
+          longDescription: updatedBlog.longDescription,
+        },
+      };
+      const result = await blogsCollection.updateOne(query, blog, options);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -77,5 +99,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`server running on port: ${port}`);
+  console.log("server running on", port);
 });
