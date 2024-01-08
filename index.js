11b2a1c4133aa9f95f9e63f46f954533362d58ef
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-01-08 13:51:30 +0600

    server updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["https://blog-zone-web.netlify.app", 'http://localhost:5173/'],
+    origin: ["https://blog-zone-web.netlify.app", 'http://localhost:5173/','http://localhost:5174/'],
     credentials: true,
   })
 );
